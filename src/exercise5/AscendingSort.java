94834195f9bfba1f268718a9240003cfe3d32e1b
AlexanderYkmv <>
2021-11-19 16:45:39 +0000

    ubung5

--- src/exercise5/AscendingSort.java
+++ src/exercise5/AscendingSort.java
@@ -11,15 +11,25 @@ public class AscendingSort {
         int numberCount = input.nextInt();
 
         int[] numbers = new int[numberCount];
+        int temp;
 
         System.out.println("Enter numbers: ");
         for (int index = 0; index < numbers.length; index++) {
             numbers[index] = input.nextInt();
         }
 
-        Arrays.sort(numbers);
+        for (int index = 0; index < numbers.length; index++) {
+          for (int j = 0; j < numbers.length -1; j++){
+            if (numbers[j] > numbers[j+1]){
+              temp = numbers[j];
+              numbers[j] = numbers[j+1];
+              numbers[j+1] = temp;
+            }
 
-        
-        System.out.println("Sorted numbers: " + Arrays.toString(numbers));
+          }
+        }
+  
+        String numbersAsString = Arrays.toString(numbers);
+        System.out.println("Sorted numbers: " + numbersAsString);
     }
 }
